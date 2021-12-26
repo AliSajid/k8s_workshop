d327ee2e9a5d72f70332a6cc59ff21675456a074
Ali Sajid Imami <Ali.Sajid.Imami@gmail.com>
2021-12-26 00:45:52 -0500

    Added the application

---
+++ src/app.py
@@ -0,0 +1,42 @@
+"""
+Test project for kubernetes workshop
+"""
+
+
+from flask import Flask, jsonify, abort, request
+import urllib.request, json, os
+
+from github import Github
+
+GITHUB_CLIENT_ID = os.getenv("GITHUB_CLIENT_ID")
+GITHUB_CLIENT_SECRET = os.getenv("GITHUB_CLIENT_SECRET")
+DEBUG = os.getenv("DEBUG")
+
+g = Github(GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET)
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    r = []
+
+    try:
+        args = request.args
+        n = int(args["n"])
+        language = args.get("language", "python")
+    except (ValueError, LookupError) as e:
+        abort(jsonify(error="No integer provided for argument 'n' in the URL"))
+
+    repositories = g.search_repositories(query=f"language:{language}")[:n]
+
+    for repo in repositories:
+        with urllib.request.urlopen(repo.url) as url:
+            data = json.loads(url.read().decode())
+        r.append(data)
+
+    return jsonify({"repos": r})
+
+
+if __name__ == "__main__":
+    app.run(debug=DEBUG)
