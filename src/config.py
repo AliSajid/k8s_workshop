e4a03610fd40bec8d1142675d9e6dd7e00a20438
Ali Sajid Imami <Ali.Sajid.Imami@gmail.com>
2022-01-17 13:35:37 -0500

    Updated the configuration for the app

--- src/config.py
+++ src/config.py
@@ -3,4 +3,4 @@ workers = multiprocessing.cpu_count() * 2 + 1
 threads = 2 * multiprocessing.cpu_count()
 
 from os import environ as env
-bind = env.get("HOST","0.0.0.0") +":"+ env.get("PORT", 5000)
+bind = env.get("HOST","0.0.0.0") +":"+ env.get("PORT", "5000")
